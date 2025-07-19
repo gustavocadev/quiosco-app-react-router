fbd881c8725fd73061ca2b2a50a6b509a43c1fb8
gustavocadev <gustavocadev@gmail.com>
2025-07-19 10:27:29 -0500

    Refactor react-router config for consistency and add Vercel preset

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,8 @@
 // react-router.config.ts
-import type { Config } from '@react-router/dev/config';
+import type { Config } from "@react-router/dev/config";
+import { vercelPreset } from "@vercel/react-router/vite";
 
 export default {
-  ssr: true,
+	ssr: true,
+	presets: [vercelPreset()],
 } satisfies Config;
