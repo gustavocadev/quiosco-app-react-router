025c06449ef744908ae41dc304aa542ee57b4451
gustavocadev <gustavocadev@gmail.com>
2025-07-19 10:27:32 -0500

    Remove Netlify plugin from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 export default defineConfig(({ command }) => ({
 	ssr: {
 		noExternal: command === "build" ? true : undefined,
 	},
-	plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), netlifyPlugin()],
+	plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 }));
